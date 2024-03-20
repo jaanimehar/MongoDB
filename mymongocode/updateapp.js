536825d9e31e273a9e6b1ba640be53046770088c
Meharban <harrysaif675@gmail.com>
2024-03-20 16:04:13 +0530

    second

--- mymongocode/updateapp.js
+++ mymongocode/updateapp.js
@@ -1,10 +1,10 @@
-const dbconnetion=require('./connectionapp');
+const dbconnetion = require('./connectionapp');
 
-const updatedata=async()=>{
-    const db=await dbconnetion();
+const updatedata = async () => {
+    const db = await dbconnetion();
 
     // const result=await db.updateOne({name:"mohan"}, {$set:{name:"sonu kumar"}});
-    const result=await db.updateMany({}, {$set:{name:"sonu kumar"}});
+    const result = await db.updateMany({}, { $set: { name: "sonu kumar" } });
 
     console.log(result);
 }
